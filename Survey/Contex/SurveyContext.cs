90f8e66e5b8f2d4e46bca120462a1174bfb6086b
Michal13m <michal13m@gmail.com>
2018-04-15 09:22:59 +0200

    init

---
+++ Survey/Contex/SurveyContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Contex
+{
+    public class SurveyContext : DbContext
+    {
+        public SurveyContext(DbContextOptions<SurveyContext> options)
+       : base(options) { }
+        public SurveyContext() { }
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<AnswerDict> AnswersDict { get; set; }
+    }
+}
